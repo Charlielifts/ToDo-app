caa41bf0b2cc5d851316e7dff8643aae37f18980
Charlielifts <benchbautis@gmail.com>
2026-03-16 01:31:38 +0800

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,16 @@
-const express = require("express")
-const cors = require("cors")
-const { Pool } = require("pg")
-
-const app = express()
-const port = 5000
-
-app.use(cors())
-app.use(express.json())
+import express from "express";
+import cors from "cors";
+import todoRoutes from "./route/todo.route.js";
 
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todo_db",
-  password: "Admin",
-  port: 5432,
-})
+const app = express();
+const port = 5000;
 
-app.get("/todos", async (req, res) => {
-  const result = await pool.query('SELECT * FROM todos ORDER BY "number of list" DESC')
-  res.json(result.rows)
-})
+app.use(cors());
+app.use(express.json());
 
-app.post("/todos", async (req, res) => {
-  const { text } = req.body
-  const result = await pool.query(
-    'INSERT INTO todos ("todo list", completed) VALUES ($1, $2) RETURNING *',
-    [text, false]
-  )
-  res.json(result.rows[0])
-})
-
-app.put("/todos/:id", async (req, res) => {
-  const { id } = req.params
-  const { completed } = req.body
-
-  await pool.query(
-    'UPDATE todos SET completed=$1 WHERE "number of list"=$2 RETURNING *',
-    [completed, id]
-  )
-
-  res.json({ message: "updated" })
-})
-
-app.delete("/todos/:id", async (req, res) => {
-  const { id } = req.params
-  await pool.query('DELETE FROM todos WHERE "number of list"=$1', [id])
-  res.json({ message: "deleted" })
-})
+app.use("/todos", todoRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 })
