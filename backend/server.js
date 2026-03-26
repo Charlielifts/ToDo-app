b91caf47446a31f041829e90e0e528827ca24727
Charlielifts <benchbautis@gmail.com>
2026-03-26 18:18:21 +0800

    updated files

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import todoRoutes from "./route/todo.route.js";
+import cookieParser from "cookie-parser"
+import pool from "./config/db.js";
+
 
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 app.use("/todos", todoRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
